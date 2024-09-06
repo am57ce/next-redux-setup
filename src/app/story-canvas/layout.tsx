8a77f60c09ed69002bfbd25452339302f37e0049
Ashar Mehmood <120026142+am57ce@users.noreply.github.com>
2024-09-07 01:13:06 +0500

    Prettier Code

--- src/app/story-canvas/layout.tsx
+++ src/app/story-canvas/layout.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import StoreProvider from '../StoreProvider';
+import React from "react";
+import StoreProvider from "../StoreProvider";
 
-export default function Home({ children }: {
-    children: React.ReactNode
-  }) {
-    return (
-        <StoreProvider>
-    <div className="flex flex-col gap-5 items-center justify-center">
+export default function Home({ children }: { children: React.ReactNode }) {
+  return (
+    <StoreProvider>
+      <div className="flex flex-col gap-5 items-center justify-center">
         <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold">Story Canvas</h1>
-        <p className="text-lg">Be Ready for <span className="underline decoration-wavy decoration-green-500">your own Story</span></p>
-        {children}
+          <h1 className="text-4xl font-bold">Story Canvas</h1>
+          <p className="text-lg">
+            Be Ready for{" "}
+            <span className="underline decoration-wavy decoration-green-500">
+              your own Story
+            </span>
+          </p>
+          {children}
         </div>
       </div>
-      </StoreProvider>
-    );
-};
+    </StoreProvider>
+  );
+}
