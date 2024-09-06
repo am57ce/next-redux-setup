8f04cae13ef075acf5159dadc5c09e54dfc5a5db
Ashar Mehmood <120026142+am57ce@users.noreply.github.com>
2024-09-06 16:59:06 +0500

    Clutter Freed + Some bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className="flex antialiased min-w-screen min-h-screen overflow-x-hidden flex-col items-center justify-between p-24">
+        {children}
+        </main>
+        </body>
     </html>
   );
 }
